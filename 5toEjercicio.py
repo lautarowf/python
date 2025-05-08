2d28293ef4e4b6734b2cb74548a36caefa1eb7cb
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-07 22:51:50 -0300

    Actualizar 5toEjercicio.py

--- 5toEjercicio.py
+++ 5toEjercicio.py
@@ -3,4 +3,4 @@ print("Ingrese la cantidad en que desea grabar (en GB)")
 GB = float(input())
 MB = GB * 1024
 MD = MB/1.44
-print("Se necesitan ", redon(MD), "Micro discos 3.5")
+print("Se necesitan ", round(MD), "Micro discos 3.5")
