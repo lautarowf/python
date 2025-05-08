e8d30e33b5e79280976d99c4041f5d79f9b81c20
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-07 22:28:35 -0300

    Actualizar 3erEjercicio.py

--- 3erEjercicio.py
+++ 3erEjercicio.py
@@ -5,7 +5,5 @@ print("Ingrese la cantidad de respuestas incorrectas")
 incorrectas = int(input())
 print("Ingrese la cantidad de respuestas en blanco")
 blanco = int(input())
-puntaje_correctas = correctas*3
-puntaje_incorrectas = incorrectas*-1
-puntaje=puntaje_correctas+puntaje_incorrectas+puntaje_blanco
+puntaje= correctas *3 + incorrectas * -1
 print("El puntaje es: ", puntaje)
