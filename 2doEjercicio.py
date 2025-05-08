b8412a2bbdc6a1c6f8dd8a9c2aae83f1991305b7
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-07 22:18:27 -0300

    Actualizar 2doEjercicio.py

--- 2doEjercicio.py
+++ 2doEjercicio.py
@@ -1,8 +1,8 @@
 #Promedio de notas
 print("Ingrese las 3 notas")
-nota1 = int(input("1ra: ")
-nota2 = int(input("2da: ")
-nota3 = int(input("3ra: ")
+nota1 = int(input("1ra: "))
+nota2 = int(input("2da: "))
+nota3 = int(input("3ra: "))
 promedio = (nota1+nota2+nota3)/3
 print ("El promedio es: ", promedio)
 
