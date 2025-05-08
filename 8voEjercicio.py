10f68c3cd386a5600a8076df1e0dfe39db49a71f
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-07 23:23:06 -0300

    Crear 8voEjercicio.py

---
+++ 8voEjercicio.py
@@ -0,0 +1,8 @@
+#Perimetro y superficie de un rectángulo 
+print("Ingrese la base del rectángulo:"
+base = float(input())
+print("Ingrese la altura del rectángulo:"
+altura = float(input())
+perímetro = base*2 + altura*2
+superficie = base * altura
+print("El perímetro es: ", perímetro, "La superficie es: ", superficie)
