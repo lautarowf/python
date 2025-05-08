83a9c3b1b513a89b35fd0989fc188bdae4781dd4
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-07 22:49:18 -0300

    Crear 5toEjercicio.py

---
+++ 5toEjercicio.py
@@ -0,0 +1,6 @@
+#Cantidad de Micro discos
+print("Ingrese la cantidad en que desea grabar (en GB)")
+GB = float(input())
+MB = GB * 1024
+MD = MB/1.44
+print("Se necesitan ", MD, "Micro discos 3.5")
