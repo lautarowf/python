958a2570aef2817d5247befa62cc338a652c9193
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-07 23:35:36 -0300

    Actualizar 9noEjercicio.py

--- 9noEjercicio.py
+++ 9noEjercicio.py
@@ -2,5 +2,5 @@
 print("Ingrese la cantidad de combustible en galones")
 galones = float(input())
 litros = galones*3.785
-precio = litros/1000
+precio = litros*1000
 print("El total a pagar es: $", precio)
