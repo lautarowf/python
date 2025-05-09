62164b01b21dd2e9af51a7c8766cfa9129887ed8
lautaro <lautarowagner@gmail.com>
2025-05-09 10:44:49 -0300

    Create 4toEjercicio.py

---
+++ ejercicios_complementarios/4toEjercicio.py
@@ -0,0 +1,5 @@
+#Calcular la velocidad
+distancia = float(input("Ingrese la distancia recorrida (m): "))
+tiempo = float(input("Ingrese el tiempo transcurrido (seg): " ))
+velocidad = distancia/tiempo
+print("La velocidad del movil es: ",velocidad, "m/seg")
