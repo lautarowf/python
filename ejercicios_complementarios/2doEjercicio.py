42d94631d0cafc8877e23ca5182fb214478a89fa
lautaro <lautarowagner@gmail.com>
2025-05-09 09:53:06 -0300

    Create 2doEjercicio.py

---
+++ ejercicios_complementarios/2doEjercicio.py
@@ -0,0 +1,4 @@
+#Perimetro triangulo equilatero
+altura = float(input("Ingrese la altura del tringulo:"))
+perimetro = 3*(2*altura/3**0.5) #formula equilatero
+print("El perimetro del triangulo es: ",perimetro)
