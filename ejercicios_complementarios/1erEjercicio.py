f0b5ce76c3fadebfdc7919f771b8ef0f19dfcab8
lautaro <lautarowagner@gmail.com>
2025-05-09 09:01:32 -0300

    Create 1erEjercicio.py

---
+++ ejercicios_complementarios/1erEjercicio.py
@@ -0,0 +1,5 @@
+#Volumen de una esfera
+Pi = 3.1416
+radio = float(input("Ingrese el radio del circulo: "))
+volumen = (4/3)*Pi*radio**3
+print("El volumen de la esfera es: ","{:.2f}".format(volumen,))
