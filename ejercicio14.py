4d8795a0c8009631c6d59bea176c934f465b99b6
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-10 00:42:42 -0300

    Actualizar ejercicio14.py

--- ejercicio14.py
+++ ejercicio14.py
@@ -1,8 +1,10 @@
 #Calculos
-numero = int(input("ingrese un numero")
-calculo = int(input("elija el calculo")
-formula = {
+numero = int(input("ingrese un numero"))
+calculo = int(input("elija el calculo"))
+funciom = {
 1:100*calculo
 2:100**calculo
 3:100/calculo
 }
+calculo = Funcion.get(numero, 0)
+print(calculo)
