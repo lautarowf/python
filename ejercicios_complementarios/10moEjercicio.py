94bd6dcab09e58f61b06b6953907c0700c7bc7d6
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-09 22:10:08 -0300

    Actualizar 10moEjercicio.py

---
+++ ejercicios_complementarios/10moEjercicio.py
@@ -0,0 +1,12 @@
+#Distancia entre dos puntos en el espacio 
+print("Ingrese las coordenadas del punto 1")
+x1 = float(input("x1= "))
+y1 = float(input("y1= "))
+z1 = float(input("z1= "))
+print("Ingrese las coordenadas del punto 2")
+x2 = float(input("x2= "))
+y2 = float(input("y2= "))
+z2 = float(input("z2= "))
+#formula: d= ((x2 - x1)^2 + (y2 - y1)^2 +(z2 -z1)^2)^0.5
+distancia = ((x2 - x1)**2 + (y2 - y1)**2 +(z2 -z1)^2)**0.5
+print("La distancia entre el punto 1 y el punto 2 es: ",round(distancia, 2))
