c045ec80dcc81d5342d97a8abf6e1eca57f9804a
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-09 22:11:31 -0300

    Actualizar 10moEjercicio.py

--- ejercicios_complementarios/10moEjercicio.py
+++ ejercicios_complementarios/10moEjercicio.py
@@ -8,5 +8,5 @@ x2 = float(input("x2= "))
 y2 = float(input("y2= "))
 z2 = float(input("z2= "))
 #formula: d= ((x2 - x1)^2 + (y2 - y1)^2 +(z2 -z1)^2)^0.5
-distancia = ((x2 - x1)**2 + (y2 - y1)**2 +(z2 -z1)^2)**0.5
+distancia = ((x2 - x1)**2 + (y2 - y1)**2 +(z2 -z1)**2)**0.5
 print("La distancia entre el punto 1 y el punto 2 es: ",round(distancia, 2))
