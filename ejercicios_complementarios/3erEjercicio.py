f3090d1911e300a8a82e672a6f4a2eaf47e29b99
lautaro <lautarowagner@gmail.com>
2025-05-14 11:47:23 -0300

    Update 3erEjercicio.py

--- ejercicios_complementarios/3erEjercicio.py
+++ ejercicios_complementarios/3erEjercicio.py
@@ -1,6 +1,6 @@
 #Cambio de Soles
-dolares = 3.25
-euros = 3.83
+DOLAR = 3.25
+EURO = 3.83
 soles = float(input("Ingrese la cantidad de Soles:"))
-print("Su cantidad en dolares es: ","{:.2f}".format(soles/dolares))
-print("Su cantidad en euros es: ","{:.2f}".format(soles/euros))
+print("Su cantidad en dolares es: ","{:.2f}".format(soles/DOLAR))
+print("Su cantidad en euros es: ","{:.2f}".format(soles/EURO))
