6fac9bef5921170efb2c49e4d3c061182fe63fda
lautaro <lautarowagner@gmail.com>
2025-05-14 11:31:44 -0300

    Create costoViaje.py

---
+++ ejercicios_complementarios/extra/costoViaje.py
@@ -0,0 +1,8 @@
+#Costo total del viaje
+recorridoEnKilometros = 250
+consumo_combusible = 0.05
+precio_combustible = 1000
+
+consumo_viaje = recorridoEnKilometros*consumo_combusible*precio_combustible
+
+print("El costo total es: $", consumo_viaje)
