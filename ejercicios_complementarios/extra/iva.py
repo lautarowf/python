d1d9712185a8c94a5902c7f638ee21bf5aa2d23e
lautaro <lautarowagner@gmail.com>
2025-05-14 09:57:52 -0300

    extras

--- ejercicios_complementarios/extra/iva.py
+++ ejercicios_complementarios/extra/iva.py
@@ -1,4 +1,4 @@
 #Precio con IVA
-precio = float(input("Ingrese el total de su compra: $"))
+precio = float(input("Ingrese el total de su compra: "))
 precioConIVA = precio * 1.21
 print("El total a abonar con IVA es de: $",precioConIVA)
---
+++ ejercicios_complementarios/extra/propina.py
@@ -0,0 +1,4 @@
+#Propina
+total = float(input("Ingrese el total de la cuenta: "))
+propina = total*0.10
+print("La propina tiene que ser: ",propina)
