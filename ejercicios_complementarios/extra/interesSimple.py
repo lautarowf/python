11bfad389d850c81a713d1bb156d020830b3004c
lautaro <lautarowagner@gmail.com>
2025-05-14 11:13:43 -0300

    Update interesSimple.py

--- ejercicios_complementarios/extra/interesSimple.py
+++ ejercicios_complementarios/extra/interesSimple.py
@@ -1,7 +1,7 @@
 #Calcular el interes simple generado por una inversion
 monto = 4500
-tasa_de_interes = 0.095
+tasa_de_interes = 0.095 #9,5%
 periodo_en_anios = 6
 
 interesSimple = monto*tasa_de_interes*periodo_en_anios
-print("El interes simple sera de: $",interesSimple)
+print("El interes simple sera de: $", interesSimple)
