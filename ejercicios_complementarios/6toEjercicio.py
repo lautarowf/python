dd2b3a1a58306ceb0f997b9f8297c69a2ca7b275
lautaro <lautarowagner@gmail.com>
2025-05-16 08:56:24 -0300

    Update 6toEjercicio.py

--- ejercicios_complementarios/6toEjercicio.py
+++ ejercicios_complementarios/6toEjercicio.py
@@ -1,5 +1,5 @@
 #Millas a KM
-milla_a_KM = 1.609344
+MILLA = 1.609344
 millas = float(input("Ingrese las millas a convertir: "))
-KM = milla_a_KM*millas
-print(round(KM, 2), "KM")
+Km = MILLA*millas
+print(round(Km, 2), "KM")
