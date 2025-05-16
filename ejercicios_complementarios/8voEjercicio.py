b381f08b1d48e6ac67f8aed01ea501bfdab34c4d
lautaro <lautarowagner@gmail.com>
2025-05-16 10:31:34 -0300

    Update 8voEjercicio.py

--- ejercicios_complementarios/8voEjercicio.py
+++ ejercicios_complementarios/8voEjercicio.py
@@ -1,7 +1,9 @@
 #Calcular el tiempo de encuentro
-velocidadA = float(input("Ingrese la velocidad de A: "))
-velocidadB = float(input("Ingrese la velocidad de B: "))
-distancia = float(input("Ingrese la distancia que los separa: "))
+velocidadA = float(input("Ingrese la velocidad de A en km/h: "))
+velocidadB = float(input("Ingrese la velocidad de B en km/h: "))
+distancia = float(input("Ingrese la distancia que los separa en metros: "))
 #Formula: Te= d/(v1 + v2)
-tiempo_de_encuentro = distancia/(velocidadA+velocidadB)
+velocidadA_en_metros = velocidadA*1000/3600
+velocidadB_en_metros = velocidadB*1000/3600
+tiempo_de_encuentro = distancia/(velocidadA_en_metros+velocidadB_en_metros)
 print("El tiempo de encuentro es: ",round(tiempo_de_encuentro, 2), " segundos")
