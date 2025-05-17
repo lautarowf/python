d5e66b7fb8ba2abea24763cdcd5afe213c7f30e7
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-17 19:14:08 -0300

    Crear complementario7.py

---
+++ estructura_sm/complementario7.py
@@ -0,0 +1,6 @@
+#Calculo de raíces relaes
+print("Dada la forma de una ecuación cuadrática: ax^2 + bx + c. Ingrese los siguientes valores:")
+a = float(input("a= "))
+b = float(input("b= "))
+c = float(input("c= "))
+d = b**2 - 4*a*
