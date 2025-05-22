c48debb0063a8aefa928a887c1b05fee8e4dda6b
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-22 19:44:55 -0300

    Actualizar complementario4.py

--- estructura_sm/complementario4.py
+++ estructura_sm/complementario4.py
@@ -4,6 +4,9 @@ dia = int(input())
 mes = int(input())
 anio = int(input())
 if dia > 0 and dia <30:
-    dia+1
-    print(dia, "/", mes, "/", anio)
+    print(dia+1, "/", mes, "/", anio)
+elif mes >0 and mes <12:
+    print(1, "/", mes+1, "/", anio)
+else:
+    print(1, "/", 1, "/", anio+1)
     
