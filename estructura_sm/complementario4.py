3d08f277cf3c6edd48769e470256fc2ee940205f
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-22 19:51:54 -0300

    Actualizar complementario4.py

--- estructura_sm/complementario4.py
+++ estructura_sm/complementario4.py
@@ -3,10 +3,14 @@ print("Ingrese la fecha de hoy:")
 dia = int(input())
 mes = int(input())
 anio = int(input())
-if dia > 0 and dia <30:
-    print(dia+1, "/", mes, "/", anio)
-elif mes >0 and mes <12:
-    print(1, "/", mes+1, "/", anio)
+if dia <30:
+   dia += 1
 else:
-    print(1, "/", 1, "/", anio+1)
+   dia = 1
+   if mes <12:
+   mes =+ 1
+   else:
+   mes = 1
+   anio =+ 1
+    
     
