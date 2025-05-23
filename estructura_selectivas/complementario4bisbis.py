a5584c4be64d3b5f3ee8cf078521bb0d323affb5
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-23 12:28:38 -0300

    Actualizar complementario4bisbis.py

--- estructura_selectivas/complementario4bisbis.py
+++ estructura_selectivas/complementario4bisbis.py
@@ -3,20 +3,13 @@ print("Ingrese la fecha de hoy:")
 dia = int(input("Dia: "))
 mes = int(input("Mes: "))
 anio = int(input("Año: "))
-if (anio % 400 == 0) or (anio % 4 == 0 and anio % 100 != 0):
-    if dia < 31:
-        if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
-            dia +=1
-            print(dia, mes, anio)
-    else:
-        dia = 1
-        mes +=1
-        print(dia,mes,anio)
-elif dia < 30:
-        if mes == 4 or mes == 6 or mes == 9 or mes == 11:
-            dia +=1
-            print(dia, mes, anio)
-        else:
-            dia = 1
-            mes +=1
-            print(dia,mes,anio)             
+
+if mes == 2:
+   if (anio % 400 == 0) or (anio % 4 == 0 and anio % 100 != 0):
+     dias_en_mes = 29
+   else:
+     dias_en_mes = 28
+elif mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+   dias_en_mes = 31
+elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+   dias_en_mes = 30
