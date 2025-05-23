946f4a4541ad24061dc58c551896ab1e759c1729
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-22 23:33:46 -0300

    Actualizar complementario5.py

--- estructura_sm/complementario5.py
+++ estructura_sm/complementario5.py
@@ -2,4 +2,10 @@
 distancia = float(input("Ingrese la distancia que separa a ambos vehículos"))
 velocidad_1 = float(input("Ingrese la velocidad del primer vehículo"))
 velocidad_2 = float(input("Ingrese la velocidad del segundo vehículo"))
+tiempo_de_encuentro = distancia/(velocidad_1 + velocidad_2)
+if velocidad_1 > 0 and velocidad_2 > 0:
+   print(tiempo_de_encuentro)
+else:
+   print(Error)
+
 
