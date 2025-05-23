18af9e8599e13a8cea3131a8bc8d0c5b1c82a744
lautaro <lautarowagner@gmail.com>
2025-05-23 11:50:54 -0300

    actt

--- estructura_selectivas/complementario4.py
+++ estructura_selectivas/complementario4.py
@@ -7,9 +7,9 @@ if dia <30:
     dia += 1
 else:
     dia = 1 
-if mes <12:
-    mes += 1
-else:
-    mes = 1
-    anio += 1
-    print(dia, mes, anio)
+    if mes <12:
+        mes += 1
+    else:
+        mes = 1
+        anio += 1
+print(dia, mes, anio)
---
+++ estructura_selectivas/complementario4bis.py
@@ -0,0 +1,38 @@
+#calcular el dia siguiente
+print("Ingrese la fecha de hoy:")
+dia = int(input("Dia: "))
+mes = int(input("Mes: "))
+anio = int(input("Año: "))
+if mes == 1 or 3 or 5 or 7 or 8 or 10 or 12:
+    if dia <31:
+        dia += 1
+    else:
+        dia = 1 
+        if mes <12:
+            mes += 1
+        else:
+            mes = 1
+            anio += 1
+print(dia, mes, anio)
+if mes == 4 or 6 or 9 or 11:
+    if dia <30:
+        dia += 1
+    else:
+        dia = 1 
+        if mes <12:
+            mes += 1
+        else:
+            mes = 1
+            anio += 1
+    print(dia, mes, anio)
+if (anio % 400 == 0) or (anio % 4 == 0 and anio % 100 != 0) and mes == 2:
+        if dia <29:
+            dia += 1
+        else:
+            dia = 1 
+            if mes <12:
+                mes += 1
+            else:
+                mes = 1
+                anio += 1
+print(dia, mes, anio)
