724f818289a7dc890181e9149b52c3b16ca5b377
lautaro <lautarowagner@gmail.com>
2025-05-23 08:33:49 -0300

    dir

--- estructura_selectivas/complementario4.py
+++ estructura_selectivas/complementario4.py
@@ -3,7 +3,15 @@ print("Ingrese la fecha de hoy:")
 dia = int(input())
 mes = int(input())
 anio = int(input())
-if dia > 0 and dia <30:
-    dia+1
-    print(dia, "/", mes, "/", anio)
+if dia <30:
+    dia += 1
+else:
+   dia = 1
+    if mes <12:
+        mes += 1
+    else:
+     mes = 1
+      anio += 1
+print(dia, mes, anio)
+    
     
