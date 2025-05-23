490cb19026bd3a32e1fb6234004f7dee523dc571
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-05-22 23:27:34 -0300

    Crear complementario5.py

---
+++ estructura_sm/complementario5.py
@@ -0,0 +1,5 @@
+#tiempo de encuentro 
+distancia = float(input("Ingrese la distancia que separa a ambos vehículos"))
+velocidad_1 = float(input("Ingrese la velocidad del primer vehículo"))
+velocidad_2 = float(input("Ingrese la velocidad del segundo vehículo"))
+
