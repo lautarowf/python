b532334c8d3a25faef3c4d52362d8cce103d6fa6
lautaro <lautarowagner@gmail.com>
2025-06-06 09:35:45 -0300

    Create calcula_interes.py

---
+++ ciclos_python/calcula_interes.py
@@ -0,0 +1,11 @@
+print("Calcula el interes")
+C = -1
+I = 0
+M = 0
+while (C>0) or (I<=0) or (I>=100) or (M<=0):
+    C = int(input("Capital: "))
+    I = int(input("Interes: "))
+    M = int(input("Tiempo en años: "))
+for i in range(M):
+    C = C*(1+I/100)
+print("Tienes", C, "soles") 
