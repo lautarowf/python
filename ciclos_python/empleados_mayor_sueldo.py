4c3b27d21f4e7173b063f366705ebb982988ef41
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-06-24 19:48:09 -0300

    Actualizar empleados_mayor_sueldo.py

--- ciclos_python/empleados_mayor_sueldo.py
+++ ciclos_python/empleados_mayor_sueldo.py
@@ -2,14 +2,17 @@ cantidad_de_empleados = 0
 contador_de_sueldos = 0
 sueldo_mayor = 0
 orden_del_mayor = 0
+
 print("Ingrese la cantidad de empleados: ")
 cantidad_de_empleados = int(input())
-while contador_de_sueldos <= cantidad_de_empleados:
-  print("Ingrese el sueldo: ")
-  sueldo = int(input())
-  cantidad_de_sueldos += 1
-  if sueldo > sueldo_mayor:
-    sueldo_mayor = sueldo
-    orden_del_mayor = contador_de_sueldos
-print("Empleado: ", orden_del_mayor,"-", sueldo_mayor)
-  
+
+while contador_de_sueldos < cantidad_de_empleados:
+    print("Ingrese el sueldo: ")
+    sueldo = int(input())
+    contador_de_sueldos += 1
+
+    if sueldo > sueldo_mayor:
+        sueldo_mayor = sueldo
+        orden_del_mayor = contador_de_sueldos
+
+print("Empleado:", orden_del_mayor, "-", sueldo_mayor)
