efa7965899e58c64bd2859575d88deef4ca4ef6e
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-06-24 19:38:01 -0300

    Crear empleados_mayor_sueldo.py

---
+++ ciclos_python/empleados_mayor_sueldo.py
@@ -0,0 +1,13 @@
+cantidad_de_empleados = 0
+camtidad_de_sueldos = 0
+sueldo_mayor = 0
+print("Ingrese la cantidad de empleados: ")
+cantidad_de_empleados = int(input())
+while cantidad_de_sueldos <= cantidad_de_empleados:
+  print("Ingrese el sueldo: ")
+  sueldo = int(input())
+  cantidad_de_sueldos += 1
+    if sueldo > sueldo_mayor:
+      sueldo_mayor = sueldo
+print("El sueldo mayor es: , sueldo_mayor"
+  
