682b3c51c941af45ff77653bda7ef2fde9d13c1b
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-06-25 02:12:27 -0300

    Actualizar complementario2.py

--- ciclos_python/complementario2.py
+++ ciclos_python/complementario2.py
@@ -1,1 +1,11 @@
-hola
+print("Suma de los dígitos de un número")
+
+numero = int(input("Ingrese un número entero positivo: "))
+suma_digitos = 0
+
+while numero > 0:
+    digito = numero % 10         # Último dígito
+    suma_digitos += digito       # Se suma a la acumulación
+    numero = numero // 10        # Se elimina el último dígito
+
+print("La suma de los dígitos es:", suma_digitos)
