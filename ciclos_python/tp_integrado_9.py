49992e6fc226006b2d8e6d623bacc149dfc299a4
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-06-25 00:13:18 -0300

    Crear tp_integrado_9.py

---
+++ ciclos_python/tp_integrado_9.py
@@ -0,0 +1,17 @@
+LIMITE = 20000
+compra = 0
+compra_total = 0
+
+print("Simulador de compras (límite diario: $20.000)")
+
+while compra_total < LIMITE:
+    compra = float(input("Ingrese el valor de su compra: "))
+    
+    if compra_total + compra > LIMITE:
+        print("⚠️ No puede realizar esta compra. Supera el límite diario de $20.000.")
+        break
+    
+    compra_total += compra
+    print(f"Total acumulado: ${compra_total:.2f}")
+
+print("\nFin del día. Total gastado:", compra_total)
