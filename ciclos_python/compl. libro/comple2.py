03e3dee937e87c77d2be4dd1ba2121f681c359a1
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-06-26 20:16:11 -0300

    Actualizar comple2.py

--- ciclos_python/compl. libro/comple2.py
+++ ciclos_python/compl. libro/comple2.py
@@ -1,7 +1,10 @@
 PRECIO_COMBUSTIBLE = 132
-litros = float(input("Ingrese cuantos litros desea cargar")
-  while litros  < 1 and litros > 50:
-      print("Ingrese un valor entre 1 y 50")
-      litros = float(input("Ingrese cuantos litros desea cargar")
+
+litros = float(input("Ingrese cuántos litros desea cargar: "))
+
+while litros < 1 or litros > 50:
+    print("Error: Ingrese un valor entre 1 y 50.")
+    litros = float(input("Ingrese cuántos litros desea cargar: "))
+
 total_a_pagar = litros * PRECIO_COMBUSTIBLE
-print("$", total_a_pagar)
+print("Total a pagar: $", total_a_pagar)
