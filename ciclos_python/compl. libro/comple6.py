b01a51334a13bf827b7985284a66dca326c70113
lautarowf <91381032+lautarowf@users.noreply.github.com>
2025-06-29 21:44:28 -0300

    Actualizar comple6.py

--- ciclos_python/compl. libro/comple6.py
+++ ciclos_python/compl. libro/comple6.py
@@ -1,1 +1,9 @@
-hola
+objetivo_diario = 0
+for dias in range (1, 8):
+  tiempo_entrenado =  float(input("Ingrese cantidad de minutos diarios de entrenamiento"))
+   if tiempo_entrenado >= 30:
+     objetivo_diario += 1
+if objetivo_diario >= 5: 
+   print("objetivo cumplido!")
+else:
+  print("no se logró el objetivo")
